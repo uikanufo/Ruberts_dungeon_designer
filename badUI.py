4af00536cf13aebff4c2de74e836a84ebafb825a
taco <arsiasaarinen@gmail.com>
2023-03-12 14:21:16 +0200

    Fixing the square function

---
+++ badUI.py
@@ -0,0 +1,130 @@
+from tkinter import *
+from tkinter import ttk, simpledialog, filedialog
+import pickle
+import os
+from project import *
+from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_DOWN
+
+
+window_width = 300
+window_height = 300
+
+class Window(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+
+        menu = Menu(self.master)
+        self.master.config(menu=menu)
+
+        fileMenu = Menu(menu)
+        fileMenu.add_command(label="Item")
+        fileMenu.add_command(label="Exit", command=self.exitProgram)
+        fileMenu.add_command(label="New project", command = self.new_project)
+        fileMenu.add_command(label="Save Project", command = self.save_project)
+        fileMenu.add_command(label="Load project", command= self.load_project)
+        menu.add_cascade(label="File", menu=fileMenu)
+
+        editMenu = Menu(menu)
+        editMenu.add_command(label="Undo")
+        editMenu.add_command(label="Redo")
+        print(Canvas.configure(self).keys())
+        menu.add_cascade(label="Edit", menu=editMenu)
+        self.current_project = False
+    
+    def new_project(self):
+        if self.current_project.__class__ == Project:
+            self.current_project.unload_canvas()
+        self.current_project = Project(root, window_height, window_width)
+        print(self.current_project.hori_line_coordinates)
+        print(self.current_project.vert_line_coordinates)
+
+    def exitProgram(self):
+        exit()
+
+    
+    def save_project(self):
+        savable_project = self.current_project.save_project()
+        
+    def load_project(self):
+        project = filedialog.askopenfilename()
+        projectbytes = open(project, "rb")
+        project = pickle.load(projectbytes)
+        project.load_project(root, window_height, window_width)
+
+
+    
+
+
+
+if os.path.isdir("Ruberts_projects") != True:
+    os.mkdir("Ruberts_projects")
+
+
+root = Tk()
+app = Window(root)
+
+
+def god_help_me(event):
+    """I copied this code from Stackoverflow in a desperate attempt to see if it does what I want"""
+    canvas = app.current_project.C
+    draft = Canvas(app) # if w is deleted, the draft is deleted
+    draft.delete(ALL) # if you use the fake canvas for other uses
+
+    concerned = canvas.find_withtag("line") # what you want
+
+    for obj in concerned: # copy on draft
+
+        # first, get the method to use
+        if canvas.type(obj) == "line": create = draft.create_line
+        # use "elif ..." to copy more types of objects
+        else: continue
+
+        # copy the element with its attributes
+        config = {opt:canvas.itemcget(obj, opt) for opt in canvas.itemconfig(obj)}
+        config["tags"] = str(obj) # I can retrieve the ID in "w" later with this trick
+        create(*canvas.coords(obj), **config)
+
+    # use coordinates relative to the canvas
+    x = canvas.canvasx(event.x)
+    y = canvas.canvasy(event.y)
+
+    item = draft.find_closest(x,y) # ID in draft (as a tuple of len 1)
+    if item: item = int( draft.gettags(*item)[0] ) # ID in w
+    else: item = None # closest not found
+
+    print(x, y)
+
+def draw_rectangle(event):
+    x = event.x
+    y = event.y
+    xstart = x-(x%5)
+    ystart = y-(y%5)
+    x0 = app.current_project.vert_line_coordinates[xstart/5][0][0]
+    y0 = app.current_project.hori_line_coordinates[ystart/5][0][1]
+
+    x1 = app.current_project.vert_line_coordinates[(xstart+5)/5][0][0]
+    y1 = app.current_project.hori_line_coordinates[(ystart+5)/5][0][1]
+    correct_coords = (x0, y0, x1, y1,)
+    
+
+    """
+    Idea:
+    Divide x coordinates by grid size
+    Round down to nearest (lower) number divisible
+    Should give the index in the list
+    Repeat for Y
+    Same for other corner, but round to higher
+    Check where lines intersect
+    """ 
+
+
+
+
+root.bind("<Button>", god_help_me)
+        
+
+root.wm_title("Tkinter window")
+
+root.mainloop()
+
--- project.py
+++ project.py
@@ -5,19 +5,25 @@ import pickle
 
 class Project:
 
+
     def draw_black_line(self, x0, y0, x1, y1):
-        return self.C.create_line(x0, y0, x1, y1, fill="black")
+        return self.C.create_line(x0, y0, x1, y1, fill="black", tags = "line")
 
     def grid_function(self, size:int, window_width, window_height):
+
+        global vert_line_coordinates
+        global hori_line_coordinates 
         hori_line = 0
         vert_line = 0
         while vert_line != floor(window_width/size):
             self.draw_black_line(0, vert_line*size, window_width, (vert_line*size)) #Horizontal lines
+            #Appends tuples with the coordinates of the particular line into the list
+            self.hori_line_coordinates.append(((0, vert_line*size), (window_width, vert_line*size),))
             vert_line += 1
         while hori_line != floor(window_height/size):
             self.draw_black_line((hori_line*size), 0, (hori_line*size), window_height) #Vertical lines
+            self.vert_line_coordinates.append(((hori_line*size, 0), (window_height, hori_line*size),))
             hori_line += 1
-    #squares = [[False for peruna in range(leveys)] for peruna in range(korkeus)]
 
     def save_project(self):
         """Useless for now"""
@@ -35,15 +41,17 @@ class Project:
         self.C.pack()
 
     def draw_square(self, mouse_coordinates):
-        self.C.create_rectangle(mouse_coordinates[0]-5,
-        mouse_coordinates[1]-5, 
-        mouse_coordinates[0]+5,
-        mouse_coordinates[1]+5)
+        self.C.create_rectangle(mouse_coordinates[0],
+        mouse_coordinates[1], 
+        mouse_coordinates[0],
+        mouse_coordinates[1])
 
 
 
     def __init__(self, root, window_height, window_width) -> None:
-
+        #These lists store the start and end points of lines as tuples of two tuples
+        self.vert_line_coordinates = []
+        self.hori_line_coordinates = []
         self.name = simpledialog.askstring("", "Name the project")
         #for vali in range(C.cget(height)/10):
         self.load_project(root, window_height, window_width)
